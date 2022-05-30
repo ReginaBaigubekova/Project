b37297d1b115c9a239055d015c45b499aad4302a
ReginaBaigubekova <70623604+ReginaBaigubekova@users.noreply.github.com>
2022-05-30 12:20:51 +0300

    The ability to learn words is added
    and everything is combined

--- phrases.py
+++ phrases.py
@@ -24,17 +24,56 @@ class Phrases(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         self.controller = controller
-        self.listbox = tk.Listbox(self, width=50, height=60, bd=10, fg="black", highlightcolor="#ebccff",
+        self.listbox = tk.Listbox(self, width=25, height=20, bd=10, fg="black", highlightcolor="#ebccff",
                                   font=("Courier", 15), selectbackground="#c9a0dc")
-        self.listbox.grid(row=0, column=1, sticky='nsew')
+        self.listbox.grid(row=0, column=2, rowspan=2, sticky='ew')
         self.phrase_list = PhraseList()
         self.phrase_list.load()
         self.listbox.insert(tk.END, *self.phrase_list.phrases)
 
+        def show_selected():
+            """
+            show the translation of the selected word by pressing the button
+            """
+            from googletrans import Translator
+            translator = Translator()
+            result = translator.translate(
+                self.listbox.get(ANCHOR), dest='ru')
+            show.config(text=result.text, fg="#9400d3", font=("Courier", 13))
+
+        # Button shows translation of the selected word
+        Button(self, text='Show Selected', command=show_selected, bg="#ffc766",
+               font=("Courier", 13)).grid(row=2, column=2, sticky='ew')
+        show = Label(self)
+        show.grid(row=3, column=2, sticky='ew')
+
+        # two functions for moving words
+        def move_to_second():
+            select_to_second = self.listbox.curselection()
+            for i in select_to_second:
+                self.lb2.insert(END, self.listbox.get(i))
+            for i in reversed(select_to_second):
+                self.listbox.delete(i)
+
+        def move_to_first():
+            select_to_first = self.lb2.curselection()
+            for i in select_to_first:
+                self.listbox.insert(END, self.lb2.get(i))
+            for i in reversed(select_to_first):
+                self.lb2.delete(i)
+
+        # create second listbox for learned words
+        self.lb2 = tk.Listbox(self, width=25, height=20, bd=10, fg="black", highlightcolor="#ebccff",
+                              font=("Courier", 15), selectbackground="#c9a0dc")
+        self.lb2.grid(row=0, column=4, rowspan=2, sticky='ew')
+        Button(self, text=">>>", command=move_to_second, bg="#99ff99").grid(row=0, column=3, rowspan=1, sticky='ew')
+        Button(self, text="<<<", command=move_to_first, bg="#ff6666").grid(row=0, column=3, rowspan=2, sticky='ew')
+
         self.fr_buttons = tk.Frame(self)
         self.fr_buttons.grid(row=0, column=0, sticky='ns')
         self.btn_book = tk.Button(
-            self.fr_buttons, text='Открыть книгу', command=lambda: self.controller.show_frame('Book'))
+            self.fr_buttons, text='Открыть книгу', command=lambda: self.controller.show_frame('Book'),
+            bg="#ffc766", font=("Courier", 13))
         self.btn_book.grid(row=1, column=0, sticky='ew', padx=5)
 
     def add_phrase(self, phrase):
